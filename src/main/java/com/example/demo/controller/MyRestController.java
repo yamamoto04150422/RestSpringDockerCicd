afec2311ec4258ea5a8519210a481e2078b7e2fb
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-06-13 21:59:17 +0900

    list add

--- src/main/java/com/example/demo/controller/MyRestController.java
+++ src/main/java/com/example/demo/controller/MyRestController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
